62751934255e8cbf122bab1351f538d0389eec92
Mac <mac@macs-macbook-pro.local>
2016-02-28 22:43:24 +0200

    Initial Commit

---
+++ ZIP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ZIP
+//
+//  Created by Mac on 28/02/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
